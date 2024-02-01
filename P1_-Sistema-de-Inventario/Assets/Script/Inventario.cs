9de6ba459e528223ae924adb80dcc5fe82f95383
fernandouteca <diseno.uteca@gmail.com>
2024-01-31 20:01:23 -0600

    Inventario version 1
    Creación de inventario con activación y desactivación

--- P1_-Sistema-de-Inventario/Assets/Script/Inventario.cs
+++ P1_-Sistema-de-Inventario/Assets/Script/Inventario.cs
@@ -1,18 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Inventario : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //El espacio esta vacio o tiene un objeto
+    private bool InventActivo;
+    
+    //Referencia al inventario
+    public GameObject Hud;
+
+    //Cuantos espacios tenemos
+    private int TodEspa;
+
+    //Cuantos espacios tenemos disponibles
+    private int EspaDispo;
+
+    //Aqui guardamos los espacios
+    private GameObject[] Espacios;
+
+    //Para asignar el espacio
+    public GameObject Espacio;
+
     void Start()
+    {
+        //Cuantos espacios tienen a partir de los hijos del inventario
+        TodEspa = Espacio.transform.childCount;
+
+        //Creamos un game object por cada espacio
+        Espacios = new GameObject[TodEspa];
+
+        //Busca los espacios
+        for (int i = 0; i < TodEspa; i++)
+        {
+            //Asignamos un game object por cada espacio
+            Espacios[i] = Espacio.transform.GetChild(i).gameObject;
+        }
+
+    }
+    private void Update()
+    {
+        //pregunta si esta activo
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                InventActivo = !InventActivo;
+            }
+
+            // Se activa o desactiva el inventario
+            if (InventActivo)
+            {
+                Hud.SetActive(true);
+            }
+            else
+            {
+                Hud.SetActive(false);
+            }
+    }
+
+    void  Seleccionar()
+    {
+
+    }
+
+    void Agarrar()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void Eliminar()
     {
-        
+
     }
 }
